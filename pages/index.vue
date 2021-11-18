c6ace6437332663b0e4ccf85069110eb89b4956a
pau7rr <pau.rosquellas@gmail.com>
2021-11-18 17:44:09 +0100

    api test

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,22 @@
 import homeImage from '~/components/home-image.vue'
 export default {
   components: { homeImage },
+  created(){
+    fetch('https://randomuser.me/api/?results=10')
+  .then( response => {
+    if(response.status == 200) {
+      return response.text();
+    } else {
+      throw "Respuesta incorrecta del servidor" 
+    }
+  })
+  .then( responseText => {
+    let users = JSON.parse(responseText).results;
+    console.log('Este es el objeto de usuarios', users);
+  })
+  .catch( err => {
+    console.log(err);
+  });
+  }
 }
 </script>
