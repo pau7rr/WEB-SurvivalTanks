bc81baab310fe2527dedd1ad11e4973c3506e0e7
pau7rr <pau.rosquellas@gmail.com>
2022-01-26 18:23:50 +0100

    middleware for check if user is logged

---
+++ pages/my-tank.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      MY TANK PAGE
+  </div>
+</template>
+
+<script>
+export default {
+    middleware: 'checkLogin',
+    layout () {
+        if (localStorage.getItem('jwt')) {
+            return 'logged'
+        }
+        return 'default' 
+    },
+}
+</script>
