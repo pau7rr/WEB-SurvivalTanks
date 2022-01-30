17d40d1f6196b034e11e58780cd87dc37476880f
Pau Rosquellas <90578037+pau7rr@users.noreply.github.com>
2022-01-30 12:34:04 +0100

    Feature/responsive navbar (#11)
    * no logger navbar menu
    
    * wip: logged navbar responsive
    
    * better responsive logged navbar
    
    * new default image for logged users

--- components/mainHeader.vue
+++ components/mainHeader.vue
@@ -1,18 +1,31 @@
 <template>
-<div class="header h-28 bg-gray-900 w-full font-sans">
+<div class="header h-auto bg-gray-900 w-full p-3 md:p-6">
     <div class="m-auto w-full h-full grid grid-cols-3 justify-items-center place-items-center">
-        <div class=" flex gap-x-5">
+
+        <nuxt-link class=" text-center flex gap-x-4" to="/">
+            <img class="  w-10" src="/icons/tank.png" alt="icon">
+            <p class=" hidden lg:inline text-white text-xl md:text-2xl lg:text-3xl font-bold">Survival Tanks</p>
+        </nuxt-link>
+
+        <div class=" hidden md:flex gap-x-5">
             <nuxt-link class=" text-white" to="/devlogs">Devlogs</nuxt-link>
             <nuxt-link class=" text-white" to="/devlogs">Team</nuxt-link>
             <nuxt-link class=" text-white" to="/devlogs">Contact Us</nuxt-link>
         </div>
-        <nuxt-link to="/">
-            <p class=" text-white text-4xl font-bold">Survival Tanks</p>
-        </nuxt-link>
-        <button class=" text-white" @click="showModal = !showModal">Play Game</button>
+        
+        <button class=" justify-self-end mr-6 text-white" @click="showModal = !showModal">Play Game</button>
+        
+        <button class=" justify-self-end mr-6 md:hidden" @click="showMenuMobile = !showMenuMobile"><img src="/icons/more.svg"></button>
+
+        <div v-if="showMenuMobile" @click="closeMenuMobile" class=" col-span-3 grid justify-items-center gap-y-3 mt-5 bg-gray-800 w-full">
+            <nuxt-link class=" text-white" to="/devlogs">Devlogs</nuxt-link>
+            <nuxt-link class=" text-white" to="/devlogs">Team</nuxt-link>
+            <nuxt-link class=" text-white" to="/devlogs">Contact Us</nuxt-link>
+        </div>
     </div>
+
     <transition name="fade">
-    <modal-signin v-if="showModal" @close="closeModal"/>
+        <modal-signin v-if="showModal" @close="closeModal"/>
     </transition>
 </div>
 </template>
@@ -22,11 +35,15 @@ export default {
     data: () => {
         return {
             showModal: false,
+            showMenuMobile: false,
         }
     },
     methods: {
         closeModal() {
             this.showModal = false
+        },
+        closeMenuMobile() {
+            this.showMenuMobile = false
         },
     },
 }
@@ -40,9 +57,10 @@ export default {
     }
 
     .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s
+        transition: opacity .5s
     }
     .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0
+        opacity: 0
     }
+
 </style>
