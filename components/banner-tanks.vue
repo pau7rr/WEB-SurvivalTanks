7aa88025515bf885a3831215e947ba3f4679f1b4
pau7rr <pau.rosquellas@gmail.com>
2022-02-10 18:22:29 +0100

    new banner tanks component

---
+++ components/banner-tanks.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="w-full h-60 bg-gray-600 flex flex-col">
+    <p class="text-white text-3xl mx-auto mt-6">A lot of combinations!</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  section {
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
