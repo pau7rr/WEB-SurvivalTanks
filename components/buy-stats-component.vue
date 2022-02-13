d13960a8bb8189244582ae2a5e2a4799a6fb98aa
pau7rr <pau.rosquellas@gmail.com>
2022-02-13 14:51:43 +0100

    buy buttons animation on hover

--- components/buy-stats-component.vue
+++ components/buy-stats-component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click.emit="$emit('buy', price, type)" class="w-24 h-24 rounded-lg bg-gray-400 border-2 border-gray-800 flex flex-col shadow-lg cursor-pointer">
+  <div @click.emit="$emit('buy', price, type)" class="box w-24 h-24 rounded-lg bg-gray-400 border-2 border-gray-800 flex flex-col shadow-lg cursor-pointer">
     <p class="m-auto"> Buy <p/>
     <p class="m-auto font-bold"> {{price}} </p>
   </div>
@@ -16,4 +16,11 @@ export default {
 
 <style scoped>
 
+  .box {
+    transition: transform .2s;
+  }
+
+  .box:hover {
+    transform: scale(1.1); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+  }
 </style>
