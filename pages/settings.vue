f6fdfa2a3c1639510813718aae15217fde06217f
pau7rr <pau.rosquellas@gmail.com>
2022-03-18 16:53:16 +0100

    wip: settings

---
+++ pages/settings.vue
@@ -0,0 +1,19 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+    middleware: 'checkLogin',
+    layout () {
+        if (localStorage.getItem('jwt')) {
+            return 'logged'
+        }
+        return 'default'
+    },
+}
+</script>
+
+<style>
+
+</style>
