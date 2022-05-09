4f707a86e6489b08b17c5787d39e69b7e2515b70
pau7rr <pau.rosquellas@gmail.com>
2022-05-09 16:22:41 +0200

    nuxt18n installed & base traductions

---
+++ lang/en-US.js
@@ -0,0 +1,36 @@
+export default {
+    header: {
+      devlogs: 'Devlogs',
+      team: 'Team',
+      contact_us: 'Contact Us',
+      log_out: 'Log Out',
+      play_game: 'Play Game',
+      leaderboard: 'Leaderboard'
+    },
+    trailer: {
+      title: 'PLAY OUR GAME',
+      subtitle: 'Best tank game of 2022',
+      button: 'DOWNLOAD GAME',
+    },
+    description: {
+      title: 'Just play right now!',
+      attribute1: 'Survival Mode',
+      description1: 'Very fun survival game mode.',
+      attribute2: 'Multiplayer Mode',
+      description2: 'New multiplayer game mode made for the players who love PVP.',
+      attribute3: 'Level Up your own Tank',
+      description3: 'Buy and improve your stats in our shop.',
+      button: 'SIGN UP',
+    },
+    banner: {
+      title: 'A lot of combinations!',
+    },
+    buy: 'Buy',
+    store: {
+      strength: 'Strength',
+      health: 'Health',
+      speed: 'Speed',
+      bombs: 'Bombs',
+      regen: 'Regen'
+    },
+}
---
+++ lang/es-ES.js
@@ -0,0 +1,36 @@
+export default {
+    header: {
+      devlogs: 'Actualizaciones',
+      team: 'Equipo',
+      contact_us: 'Contacto',
+      log_out: 'Cerrar Sesión',
+      play_game: 'Juega Ya',
+      leaderboard: 'Ranking'
+    },
+    trailer: {
+      title: 'JUEGA A NUESTRO JUEGO',
+      subtitle: 'El mejor juego de tanques de 2022',
+      button: 'DESCARGAR JUEGO',
+    },
+    description: {
+      title: 'Juega ahora mismo!',
+      attribute1: 'Modo Supervivencia',
+      description1: 'Modo muy divertido de supervivencia.',
+      attribute2: 'Modo Multijugador',
+      description2: 'Nuevo modo multijugador hecho para la gente que ama el PVP.',
+      attribute3: 'Mejora tu tanque',
+      description3: 'Compra y mejora tu propio tanque en la tienda.',
+      button: 'REGISTRAR',
+    },
+    banner: {
+      title: 'Muchas combinaciones!',
+    },
+    buy: 'Comprar',
+    store: {
+        strength: 'Fuerza',
+        health: 'Vida',
+        speed: 'Velocidad',
+        bombs: 'Bombas',
+        regen: 'Regeneracion'
+    },
+}
