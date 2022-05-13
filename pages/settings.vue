8109b9a50dcd5a04c2e4992ab5c8325554f86496
pau7rr <pau.rosquellas@gmail.com>
2022-05-13 17:24:42 +0200

    change password workings

--- pages/settings.vue
+++ pages/settings.vue
@@ -24,13 +24,13 @@
             <div class="flex-1 flex flex-col md:flex-row">
                 <div class="w-full flex-1 mx-2">
                     <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
-                        <input v-model="password" type="password" placeholder="Password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
+                        <input v-model="actualPassword" type="password" placeholder="Actual Password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
                 </div>
                 <div class="w-full flex-1 mx-2">
                     <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
-                        <input type="password" placeholder="Confirm password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
+                        <input v-model="password" type="password" placeholder="Confirm password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
                 </div>
-                <button class="text-sm  mx-2 w-32 h-10  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+                <button @click="changePassword" class="text-sm  mx-2 w-32 h-10  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
                         bg-teal-100 
                         text-teal-700 
                         border duration-200 ease-in-out 
@@ -55,6 +55,7 @@ export default {
     data: () => {
         return {
             username: '',
+            actualPassword: '',
             password: '',
         }
     },
@@ -76,12 +77,40 @@ export default {
                 })
                 .then(function(data) {
                     const parseData = JSON.parse(data)
-                    console.log(parseData);
+                    
+                    if (parseData.success) location.reload()
                 })
                 .catch(function(err) {
                     console.error(err);
-                });
-            
+                });      
+        },
+        changePassword() {
+            console.log(this.username);
+
+            fetch(process.env.API_BASE_URL + 'api/user/updatePassword', {
+                method: 'PUT',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+                },
+                body: JSON.stringify({oldPassword: this.actualPassword, newPassword: this.password}),
+                })
+                .then(function(response) {
+                    return response.text();
+                })
+                .then(function(data) {
+                    const parseData = JSON.parse(data)
+                    
+                    if (parseData.success){
+                        localStorage.removeItem('jwt')
+                        localStorage.removeItem('userTankID')
+                        location.reload()
+                    }
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });      
         }
     }
 }
