be2b6008c0d367faad8395dc818ee09304729cce
pau7rr <pau.rosquellas@gmail.com>
2022-05-13 16:24:16 +0200

    wip: settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,5 +1,46 @@
 <template>
-  <section></section>
+  <div class="p-5">
+    <div class="mt-2">
+        <div class="flex flex-col md:flex-row border-b border-gray-200 pb-4 mb-4">
+            <div class="w-64 font-bold h-6 mx-2 mt-3 text-gray-800">Change Username:</div>
+            <div class="flex-1 flex flex-col md:flex-row">
+                <div class="w-full flex mx-2">
+                    <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <input placeholder="Username" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> 
+                    </div>
+                    <button class="text-sm  mx-2 w-32 h-10  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+                        bg-teal-100 
+                        text-teal-700 
+                        border duration-200 ease-in-out 
+                        border-teal-600 transition">
+                        Change
+                    </button>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col md:flex-row border-b border-gray-200 pb-4 mb-4">
+            <div class="w-64 font-bold h-6 mx-2 mt-3 text-gray-800">Change Password:</div>
+            <div class="flex-1 flex flex-col md:flex-row">
+                <div class="w-full flex-1 mx-2">
+                    <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <input type="password" placeholder="Password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
+                </div>
+                <div class="w-full flex-1 mx-2">
+                    <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <input type="password" placeholder="Confirm password" class="p-1 px-2 appearance-none outline-none w-full text-gray-800 "> </div>
+                </div>
+                <button class="text-sm  mx-2 w-32 h-10  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+                        bg-teal-100 
+                        text-teal-700 
+                        border duration-200 ease-in-out 
+                        border-teal-600 transition">
+                        Change
+                    </button>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
