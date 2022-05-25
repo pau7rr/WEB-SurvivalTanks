646d1ea972faefd608191cc15a2d20b2e580a5dd
pau7rr <pau.rosquellas@gmail.com>
2022-05-25 09:01:10 +0200

    leaderboard responsive

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -1,11 +1,16 @@
 <template>
   <section class="flex flex-col w-full h-full bg-kristal bg-cover">
-        <section class="md:flex justify-center gap-x-5 mt-10">
+        <section class="md:flex justify-center gap-x-5 mt-10 self-center">
             <section v-for="(user, index) in topRankingList" :key="user.username">
                 <top-ranking-slot :username="user.username" :round="user.highest_round" :games="user.games_played" :position="index"/>
             </section>       
         </section>
-        <section class="flex flex-col gap-y-8 mt-11">
+        <section class=" lg:hidden justify-center flex flex-col gap-x-5 gap-y-6 mt-10 self-center">
+            <section v-for="(user, index) in rankingList" :key="user.username">
+                <top-ranking-slot :username="user.username" :round="user.highest_round" :games="user.games_played" :position="index + 3"/>
+            </section>       
+        </section>
+        <section class="hidden lg:flex lg:flex-col gap-y-8 mt-11">
             <section class="" v-for="(user, index) in rankingList" :key="user.username">
                 <ranking-slot class="mx-auto" :username="user.username" :round="user.highest_round" :kills="user.kills" :games="user.games_played" :position="index"/>
             </section>  
